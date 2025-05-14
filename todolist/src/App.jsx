476d0bed235bf5335f8d87dbb947e9b109bb28f1
Kalash Satypal <Kalashsatypal@gmail.com>
2025-05-14 07:24:39 +0530

    1st

---
+++ todolist/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <h1>To Do List</h1>
+      <div className='input container'>
+        <input type="text" />
+        <button>+</button>
+      </div>
+    </main>
+  );
+}
+
+export default App
